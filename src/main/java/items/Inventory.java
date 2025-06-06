c59bcf43e4e0170a4f1b99e533053429d50b1332
TerseGarlic <141645928+TerseGarlic@users.noreply.github.com>
2025-06-05 21:49:01 -0400

    pushing to the fork for ADT assignment.

--- src/main/java/items/Inventory.java
+++ src/main/java/items/Inventory.java
@@ -27,9 +27,8 @@ public class Inventory
      */
     public static void mergeStacks(ItemStack lhs, ItemStack rhs)
     {
-        // lhs needs to have items added to it.
-        // rhs's size is needed
-        // lhs.????(rhs.????)
+        lhs.addItems(rhs.size());
+        // we do not need to change the Item in lhs
     }
 
     /**
@@ -93,8 +92,7 @@ public class Inventory
      */
     public boolean isFull()
     {
-        // Replace the next line
-        return false;
+        return this.slots.currentSize == this.capacity;
     }
 
     /**
@@ -117,8 +115,15 @@ public class Inventory
      */
     public ItemStack findMatchingItemStack(ItemStack key)
     {
-        // Add the necessary sequential search loop
+        LinkedList.Node<ItemStack> current = this.slots.head;
 
+        while (current != null) {
+            if (current.data.equals(key)) {
+                return current.data;
+            }
+
+            current = current.next;
+        }
         return null;
     }
 
@@ -130,6 +135,18 @@ public class Inventory
     public void addItemStackNoCheck(ItemStack toAdd)
     {
         LinkedList.Node<ItemStack> newNode = new LinkedList.Node<>(toAdd);
+
+        if (this.slots.head == null) {
+            this.slots.head = newNode;
+        } else {
+            LinkedList.Node<ItemStack> current = this.slots.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        this.slots.currentSize++;
 
         // Use the appendNode/add logic from Review 1 as your starting point
         // Once we reach this function... we know that `toAdd` must be stored
